39b62cb39fdc6d5f3b227df7b4f38eb80695a8e3
gscharly <charlygs94@gmail.com>
2020-10-17 11:29:52 +0200

    BEINF: random forest and gradient boosting with metrics

---
+++ scripts/models/ltr_gbt/metrics.py
@@ -0,0 +1,55 @@
+# Scripts imports
+from scripts.models.metrics_experiment import MetricsExperiment
+from scripts.models.ltr.train import LTRTrain
+import scripts.utils.ml_utils as ml_utils
+import scripts.utils.visualizations as vis_utils
+
+# DS imports
+import pandas as pd
+
+# Other scripts
+import os
+import pickle
+from typing import Dict
+
+
+class LTRGBTMetrics(MetricsExperiment):
+
+    def __init__(self, train_exp: LTRTrain):
+        super().__init__()
+        self.train_exp = train_exp
+        self.METRICS_PATH = {data_type: f'{self.train_exp.path}/{data_type}_metrics.pickle'
+                             for data_type in self.DATA_TYPES}
+
+    def _load_data(self, data_type: str) -> pd.DataFrame:
+        if data_type == 'train':
+            return self.train_exp.train_data()
+        elif data_type == 'validation':
+            return self.train_exp.ltr.read_validation()
+        else:
+            return self.train_exp.ltr.read_test()
+
+    @property
+    def metrics_path(self):
+        return self.METRICS_PATH
+
+    def metrics(self, data_type: str) -> Dict:
+        model = self.train_exp.read_model()
+        df = self._load_data(data_type)
+        X = self.train_exp.preprocess_data(df)
+        y_true = X[self.train_exp.target_col]
+        X = X.drop(self.train_exp.target_col, axis=1)
+        y_pred = model.predict(X)
+        return ml_utils.regression_metrics(y_true, y_pred)
+
+    def get_metrics(self, data_type: str) -> Dict:
+        path_2_read = self.METRICS_PATH[data_type]
+        print('Reading metrics from', path_2_read)
+        if os.path.exists(path_2_read):
+            return pickle.load(open(path_2_read, 'rb'))
+        else:
+            raise ValueError(f"{path_2_read} does not exist; please execute metrics")
+
+    def show_metrics(self, data_type: str):
+        metrics = self.get_metrics(data_type)
+        vis_utils.plot_regression_metrics(metrics)
---
+++ scripts/models/ltr_gbt/train.py
@@ -0,0 +1,105 @@
+# Scripts
+from scripts.models.train_experiment import TrainExperiment
+from scripts.extractive_summary.ltr.ltr_features_targets import LTRFeaturesTargets
+
+# DS
+import pandas as pd
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+
+# Other
+from typing import List, Dict
+
+
+class LTRGBTTrain(TrainExperiment):
+    MODEL_TYPE = 'ltr_gbt'
+    TARGET_COL = 'score'
+    RANDOM_SEED = 10
+    N_JOBS = 5
+
+    def __init__(self, ltr_params: Dict, **train_exp_params):
+        super().__init__(**train_exp_params)
+        self.ltr_params = ltr_params
+        self.ltr = LTRFeaturesTargets(**ltr_params)
+
+    def config(self) -> Dict:
+        config_dict = {
+            'cv': self.cv,
+            'opt_metric': self.opt_metric if self.opt_metric else '',
+            'features': sorted(self.features)
+        }
+        config_dict.update(self.model_params)
+        config_dict.update(self.ltr_params)
+        return config_dict
+
+    @property
+    def model_type(self) -> str:
+        return self.MODEL_TYPE
+
+    @property
+    def target_col(self) -> str:
+        return self.TARGET_COL
+
+    @property
+    def features_cols(self) -> List[str]:
+        return self.features
+
+    def train_data(self) -> pd.DataFrame:
+        """Returns train data as a pandas df"""
+        return self.ltr.read_train()
+
+    def preprocess_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        - Delete players_importance and recategorize n_players
+        - Delete advantage and equalize
+        - Sum length and n_stop
+        - Delete sim_previous and position
+        :param df:
+        :return:
+        """
+        # Number of players
+        print('Categorizing n_players...')
+        df['n_players_cat'] = df['n_players'].apply(lambda x: 'no_player' if x == 0 else 'one_player'
+                                                    if x == 1 else 'more_than_one_player')
+        # Total length
+        print('Computing new length...')
+        df['total_length'] = df['length'] + df['n_stop']
+        drop_cols = set(df.columns).difference(set(self.features))
+        print('Dropping', drop_cols)
+        df_sel = df[self.features + [self.target_col]].copy()
+        return df_sel
+
+    def pipeline(self) -> Pipeline:
+        """Define the model's pipeline"""
+        # Dummy cat features
+        cat_pipeline = Pipeline(steps=[
+            ('encoder', OneHotEncoder(drop='first',
+                                      categories=[cat_list for cat_list in self.cat_features_dict.values()]))
+        ])
+        preprocessor = ColumnTransformer(transformers=[('cat', cat_pipeline, self.cat_features)],
+                                         remainder='passthrough')
+
+        if self.cv:
+            print(f'Using cv with {self.cv} folds optimizing {self.opt_metric}')
+            rf = GradientBoostingRegressor(random_state=self.RANDOM_SEED)
+            rf = GridSearchCV(estimator=rf, param_grid=self.model_params, scoring=self.opt_metric, cv=self.cv,
+                              n_jobs=self.N_JOBS)
+        else:
+            rf = GradientBoostingRegressor(random_state=self.RANDOM_SEED, **self.model_params)
+        pipe = Pipeline(steps=[
+            ('preprocessing', preprocessor),
+            ('model', rf)
+        ])
+        return pipe
+
+    def model_out(self, pipeline: Pipeline) -> pd.DataFrame:
+        model = pipeline['model']
+        best_model = model.best_estimator_ if self.cv else model
+        dummy_cols = pipeline['preprocessing'].named_transformers_['cat']['encoder'].get_feature_names()
+        cols = list(dummy_cols) + self.num_features
+        feats = {feature: importance for feature, importance in zip(cols, best_model.feature_importances_)}
+        importances = pd.DataFrame.from_dict(feats, orient='index').rename(columns={0: 'Gini-importance'})
+        return importances
